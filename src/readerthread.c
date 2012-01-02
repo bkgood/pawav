a777d88946e200fb054b842461449df8a9787564
Bill Good <bkgood@gmail.com>
2012-01-02 17:17:26 -0600

    Implementing ReaderThread

--- src/readerthread.c
+++ src/readerthread.c
@@ -1,18 +1,66 @@
 #include <stdlib.h>
 
 #include "readerthread.h"
+#include "reader.h"
 #include "pa_ringbuffer.h"
 
+/** Private data for ReaderThread. */
 typedef struct {
-    PaUtilRingBuffer *rb;
-    pthread_t *thread;
-    pthread_cond_t *wait;
+    Reader r;
+    PaUtilRingBuffer rb;
+    pthread_t thread;
+    pthread_mutex_t lock;
+    pthread_cond_t wait;
 } ReaderThreadPrivate;
 
-bool create_readerthread(ReaderThread **rt)
+static void *readerthread_run(void *vrt);
+
+bool readerthread_init(ReaderThread *rt)
 {
-    *rt = calloc(1, sizeof(ReaderThread));
     ReaderThreadPrivate *p = calloc(1, sizeof(ReaderThreadPrivate));
-    (*rt)->p = p;
+    if (!p) return false;
+
+    rt->p = p;
     return true;
+}
+
+bool readerthread_start(ReaderThread *rt)
+{
+    ReaderThreadPrivate *p = rt->p;
+    int err;
+    if ((err = pthread_mutex_init(&p->lock, NULL))) {
+        return false;
+    }
+    if ((err = pthread_cond_init(&p->wait, NULL))) {
+        return false;
+    }
+
+    if ((err = pthread_create(&p->thread, NULL, readerthread_run, rt))) {
+        return false;
+    }
+    return true;
+}
+
+void readerthread_join(ReaderThread *rt)
+{
+    ReaderThreadPrivate *p = rt->p;
+
+    pthread_join(p->thread, NULL);
+}
+
+void readerthread_destroy(ReaderThread *rt)
+{
+    ReaderThreadPrivate *p = rt->p;
+    pthread_mutex_destroy(&p->lock);
+    pthread_cond_destroy(&p->wait);
+}
+
+/* static methods */
+static void *readerthread_run(void *vrt)
+{
+    ReaderThread *rt = vrt;
+    ReaderThreadPrivate *p = rt->p;
+    (void) p;
+
+    return NULL;
 }
--- src/readerthread.h
+++ src/readerthread.h
@@ -1,21 +1,58 @@
 #include <pthread.h>
 #include <stdbool.h>
 
+#include "pa_ringbuffer.h"
+#include "reader.h"
+
+#ifndef READERTHREAD_H
+#define READERTHREAD_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * A thread which reads data from a Reader into a PaUtilRingBuffer.
+ */
 typedef struct {
-    const char *filename;
+    /** Reader to read from. */
+    Reader *r;
+    /** PaUtilRingBuffer to read data into. */
+    PaUtilRingBuffer *rb;
+    /** Private data. */
     void *p;
 } ReaderThread;
 
-bool create_readerthread(ReaderThread **rt);
-bool start_readerthread(ReaderThread *rt);
-bool stop_readerthread(ReaderThread *rt);
-void destroy_readerthread(ReaderThread **rt);
+/**
+ * Initializes a ReaderThread.
+ *
+ * \returns true on success
+ * \returns false on out of memory
+ */
+bool readerthread_init(ReaderThread *rt);
+
+/**
+ * Starts a ReaderThread.
+ *
+ * \returns true on success
+ * \returns false on failure (pthread error)
+ */
+bool readerthread_start(ReaderThread *rt);
+
+/**
+ * Joins the calling process to the ReaderThread.
+ *
+ * May return immediately if the thread has already ended.
+ */
+void readerthread_join(ReaderThread *rt);
+
+/**
+ * Destroys a ReaderThread.
+ */
+void readerthread_destroy(ReaderThread *rt);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif

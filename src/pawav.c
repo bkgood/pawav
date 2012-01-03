b44b5d4811690f0fc86f9c110de8dd611fc30c5f
Bill Good <bkgood@gmail.com>
2012-01-03 14:58:30 -0600

    Add ReaderThread test function to pawav

--- src/pawav.c
+++ src/pawav.c
@@ -1,7 +1,12 @@
+#include <math.h>
 #include <stdio.h>
 #include <pulse/pulseaudio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "reader.h"
+#include "readerthread.h"
+#include "pa_ringbuffer.h"
 
 int run_reader(const char *f)
 {
@@ -21,6 +26,46 @@ int run_reader(const char *f)
     return 0;
 }
 
+int run_readerthread(const char *f)
+{
+    Reader r;
+    ReaderThread rt;
+    PaUtilRingBuffer rb;
+
+    size_t buffer_size = 8192;
+    reader_init(&r);
+    r.filename = f;
+    r.n = buffer_size;
+    reader_open(&r);
+    assert(log2(r.channels) == (int) log2(r.channels));
+    int16_t *rb_data = calloc(buffer_size * r.channels, sizeof(int16_t));
+    PaUtil_InitializeRingBuffer(&rb, sizeof(int16_t), buffer_size * r.channels,
+        rb_data);
+    readerthread_init(&rt);
+    rt.r = &r;
+    rt.rb = &rb;
+    readerthread_start(&rt);
+
+    sleep(1);
+    while (rt.running) {
+//        printf("trashing %d items\n", PaUtil_GetRingBufferReadAvailable(&rb));
+        PaUtil_AdvanceRingBufferReadIndex(&rb,
+                PaUtil_GetRingBufferReadAvailable(&rb));
+        printf("command wake\n");
+        readerthread_wake(&rt);
+    }
+    printf("joining\n");
+    readerthread_join(&rt);
+    /* readerthread is done */
+    readerthread_destroy(&rt);
+    reader_close(&r);
+    reader_destroy(&r);
+    free(rb_data);
+    return 0;
+}
+
+
+
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
@@ -30,5 +75,5 @@ int main(int argc, char *argv[])
     setenv("PULSE_PROP_application.name", "pawav", 1);
     setenv("PULSE_PROP_media.role", "music", 1);
 
-    return run_reader(argv[1]);
+    return run_readerthread(argv[1]);
 }
